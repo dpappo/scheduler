e1067931187e094172cc4ec7817a4a58f8066c55
Dan Pappo <dpappo@gmail.com>
2021-03-15 16:01:58 -0400

    interviewlistitem pass stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import "components/InterviewerList.scss"
+import InterviewerListItem from "components/InterviewerListItem";
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png"},
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png", selected: true },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+
+export default function InterviewerList(props) {
+  
+  //const [interviewerState, setInterviewer] = useState("")
+
+  const listInterviewers = interviewers.map((interviewer) => 
+    <InterviewerListItem
+      key={interviewer.id}
+      {...interviewer}
+      selected={interviewer.id === props.interviewer} 
+      />
+  );
+
+  return (<section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">{listInterviewers}</ul>
+</section>)
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,19 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
   
+//const [interviewer, setInterviewer] = useState(props.interviewer)
+
   const itemClass = classNames("interviewers__item",
   {"interviewers__item--selected": props.selected})
   
   return (<li className={itemClass} onClick={() => props.setInterviewer(props.name)}>
-  <img
-    className="interviewers__item-image"
-    src={props.avatar}
-    alt={props.name}
-  />
-  {props.name}
+  <img className="interviewers__item-image" src={props.avatar} alt={props.name} />
+  {props.selected && props.name}
 </li>)
 
 }
