49014420aa8237b473e4fe570def4088bcbf62e1
Dan Pappo <dpappo@gmail.com>
2021-03-15 14:05:05 -0400

    daylist error testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,6 +2,16 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require('classnames');
 
+const formatSpots = function(spots) {
+  if (spots > 1) {
+    return spots + " spots remaining"
+  } else if (spots === 0) {
+    return "no spots remaining"
+  } else {
+    return spots + " spot remaining"
+  }
+}
+
 export default function DayListItem(props) {
   let listClass = classNames("day-list__item",{
     "day-list__item--selected": props.selected,
@@ -11,7 +21,7 @@ export default function DayListItem(props) {
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   )
 }
