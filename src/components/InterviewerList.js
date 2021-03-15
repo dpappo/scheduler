7c4599c81f95256124b779dfe1fe3e4f65c0eb40
Dan Pappo <dpappo@gmail.com>
2021-03-15 18:37:05 -0400

    dynamic list state

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,7 +5,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 const interviewers = [
   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png"},
   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png", selected: true },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png"},
   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
@@ -13,13 +13,14 @@ const interviewers = [
 
 export default function InterviewerList(props) {
   
-  //const [interviewerState, setInterviewer] = useState("")
+  const [selectedInterviewer, setInterviewer] = useState()
 
   const listInterviewers = interviewers.map((interviewer) => 
     <InterviewerListItem
       key={interviewer.id}
       {...interviewer}
-      selected={interviewer.id === props.interviewer} 
+      selected={interviewer.id === selectedInterviewer} 
+      setInterviewer={setInterviewer}
       />
   );
 
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,12 +4,13 @@ import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
   
-//const [interviewer, setInterviewer] = useState(props.interviewer)
-
   const itemClass = classNames("interviewers__item",
   {"interviewers__item--selected": props.selected})
   
-  return (<li className={itemClass} onClick={() => props.setInterviewer(props.name)}>
+  return (<li className={itemClass} onClick={() => {
+      props.setInterviewer(props.id)
+      
+      }}>
   <img className="interviewers__item-image" src={props.avatar} alt={props.name} />
   {props.selected && props.name}
 </li>)
