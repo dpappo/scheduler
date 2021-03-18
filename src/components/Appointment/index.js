ae5598fdcfb02452648566f6588dcffe6e03d371
Dan Pappo <dpappo@gmail.com>
2021-03-18 14:20:20 -0400

    transition state for custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,33 @@ import "./styles.scss";
 import Header from './Header'
 import Show from './Show'
 import Empty from './Empty'
+import useVisualMode from "../../hooks/useVisualMode"
+import Form from './Form'
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return <article className="appointment">
   <Header time={props.time} />
+
+  {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+  {mode === SHOW && (
+    <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+    />
+  )}
+  {mode === CREATE && <Form interviewers={[]} onCancel={() => transition(EMPTY)} />}
+
   
-  {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+  {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />} */}
 
   </article>
 }
