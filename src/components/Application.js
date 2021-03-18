95112a9b28da3121a2082347b6bc6a6b030e379d
Dan Pappo <dpappo@gmail.com>
2021-03-18 09:39:54 -0400

    add Promise all

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,28 +14,19 @@ export default function Application(props) {
     interviewers: []
   })
   
-  useEffect(() =>{
-    axios.get("http://localhost:8001/api/appointments")
-      .then(response => {
-        setAppointments(response.data)
-      })
+  useEffect(() => {
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
+    })
+    
   }, [])
 
-  useEffect(() =>{
-    axios.get("http://localhost:8001/api/days")
-      .then(response => {
-        setDays(response.data)
-      })
-  }, [])
-
-  useEffect(() =>{
-    axios.get("http://localhost:8001/api/interviewers")
-      .then(response => {
-        setInterviewer(response.data)
-      })
-  }, [])
-
-  // console.log("state: ", state)
+  console.log("state: ", state)
 
   const dailyAppointments = getAppointmentsForDay(state, state.currentDay);
 
